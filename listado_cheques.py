b48f7d47a87cc3b84c2a196b4bd8b6c097842813
porlanagustin <porlan.agustin@gmail.com>
2022-07-03 18:29:52 -0300

    date1

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,10 +1,10 @@
 import csv
-from datetime import datetime
+from datetime import date, datetime
 
 
 # from auxiliar import Estado
 
-newdate = datetime.datetime.now()
+newdate = datetime.now()
 
 def buscadorCheques(NombreCSV,DNIcliente,Salida,TipoCheque):
     
@@ -16,7 +16,7 @@ def buscadorCheques(NombreCSV,DNIcliente,Salida,TipoCheque):
                     if Salida == "PANTALLA":
                         print([NroCheque,CodigoBanco,CodigoScurusal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado])
                     else:
-                        with open('info.csv','a',newline='') as Info:
+                        with open(f"{DNIcliente}{newdate}",'w') as Info:
                             writer = csv.writer(Info)
                             writer.writerow([NroCheque,CodigoBanco,CodigoScurusal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado])
                     
@@ -27,7 +27,7 @@ NombreCSV="cheques.csv"
 DNIcliente=int(input("Ingrese su DNI:"))
 Salida = input("Ingrese Salida (PANTALLA O CSV)")
 TipoCheque=input("Ingrese Tipo Cheque (EMITIDO O DEPOSITADO):")
-buscadorCheques(NombreCSV,DNIcliente,TipoCheque)
+buscadorCheques(NombreCSV,DNIcliente,Salida, TipoCheque)
 # buscadorCheques("cheques.csv",23665789,"EMITIDO")
 
 
