820228c87cb4fb5fba5b678138a554b2147990d2
JOAQUINESTEVEZ <joaquin2001estevez@gmail.com>
2022-07-03 19:06:35 -0300

    TITULO CSV

--- listado_cheques.py
+++ listado_cheques.py
@@ -4,10 +4,10 @@ from datetime import date, datetime
 
 # from auxiliar import Estado
 
-newdate = datetime.now()
+newdate = date.today()
 
 def buscadorCheques(NombreCSV,DNIcliente,Salida,TipoCheque):
-    
+    contador = 1
     with open(NombreCSV,'r') as file:
         reader = csv.reader(file)
         for NroCheque,CodigoBanco,CodigoScurusal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado in reader:
@@ -16,8 +16,12 @@ def buscadorCheques(NombreCSV,DNIcliente,Salida,TipoCheque):
                     if Salida == "PANTALLA":
                         print([NroCheque,CodigoBanco,CodigoScurusal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado])
                     else:
-                        with open(f"{DNIcliente}{newdate}",'w') as Info:
+                        with open(f"{DNIcliente}{newdate}.csv",'a') as Info:
+                
                             writer = csv.writer(Info)
+                            if contador == 1:
+                                writer.writerow(["NroCheque,CodigoBanco,CodigoScurusal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado"])
+                                contador += 1   
                             writer.writerow([NroCheque,CodigoBanco,CodigoScurusal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado])
                     
                     
@@ -25,7 +29,7 @@ def buscadorCheques(NombreCSV,DNIcliente,Salida,TipoCheque):
 
 NombreCSV="cheques.csv"
 DNIcliente=int(input("Ingrese su DNI:"))
-Salida = input("Ingrese Salida (PANTALLA O CSV)")
+Salida = input("Ingrese Salida (PANTALLA O CSV):")
 TipoCheque=input("Ingrese Tipo Cheque (EMITIDO O DEPOSITADO):")
 buscadorCheques(NombreCSV,DNIcliente,Salida, TipoCheque)
 # buscadorCheques("cheques.csv",23665789,"EMITIDO")
