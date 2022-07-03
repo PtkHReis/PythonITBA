f44f14e012b2f4d642b866b33bf292e7cd6f8557
Atravieso23 <agustintravieso@gmail.com>
2022-07-03 17:47:24 -0300

    Cambios

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,36 +1,30 @@
 import csv
 
-info=[
-
-]
 
 
 
 # from auxiliar import Estado
 
-def buscadorCheques(NombreCSV,DNIcliente,TipoCheque):
+def buscadorCheques(NombreCSV,DNIcliente,Salida,TipoCheque):
     
     with open(NombreCSV,'r') as file:
         reader = csv.reader(file)
         for NroCheque,CodigoBanco,CodigoScurusal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado in reader:
             if DNI == str(DNIcliente):
                 if Tipo == TipoCheque:
-                    # with open('info.csv','a',newline='') as Info:
-                    #     writer = csv.writer(Info)
-                    #     writer.writerow([NroCheque,CodigoBanco,CodigoScurusal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado])
-                    print([NroCheque,CodigoBanco,CodigoScurusal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado])
-                    info.append([NroCheque,CodigoBanco,CodigoScurusal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado])
-
-with open("info.csv","w",newline="") as file:
-    writer=csv.writer(file,delimiter=";")
-    writer.writerows(info)
+                    with open('info.csv','a',newline='') as Info:
+                        writer = csv.writer(Info)
+                        writer.writerow([NroCheque,CodigoBanco,CodigoScurusal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado])
+                    
+                    if Salida == "PANTALLA":
+                        print([NroCheque,CodigoBanco,CodigoScurusal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Tipo,Estado])
+                   
 
 NombreCSV="cheques.csv"
 DNIcliente=int(input("Ingrese su DNI:"))
 TipoCheque=input("Ingrese Tipo Cheque (EMITIDO O DEPOSITADO):")
 buscadorCheques(NombreCSV,DNIcliente,TipoCheque)
 # buscadorCheques("cheques.csv",23665789,"EMITIDO")
-print(info)
 
 
 
